b9c669558600740f45d3df98193dcff1dc5738aa
Wannapa047 <153887983+Wannapa047@users.noreply.github.com>
2024-06-19 13:40:56 +0700

    Submit Assignment1-9

--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,18 @@
 # running a function with arguments in another thread
+from time import sleep ,ctime
+from threading import Thread
+
+#a custom function that blocks for a moment
+def task(sleep_time,message):
+    #block for a moment
+    sleep(sleep_time)
+    #display a message
+    print(f'{ctime()}{message}' )
+
+#Create a thread
+thread = Thread(target=task,args=(1.5, 'New message from another thread'))
+#Run the thread
+thread.start()
+#Wait for the thread to finish 
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
