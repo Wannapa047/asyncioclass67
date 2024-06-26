71694320ddf802d06b55cb7ceb2dda701a79e823
Wannapa047 <153887983+Wannapa047@users.noreply.github.com>
2024-06-26 09:43:30 +0700

    ส่งการบ้าน

--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,24 @@
 # extending the Thread class
+from time import sleep, ctime
+from threading import Thread
+
+#custom thread class
+class CustomThread(Thread):
+    #override the run function
+    def run(self):
+        #block for a moment
+        sleep(1)
+        #Display a message
+        print(f'{ctime()} This is coming from another thread')
+
+#Create a thread
+thread = CustomThread()
+#Run the thread
+thread.start()
+'''
+Thread 2nd
+'''
+
+#Wait for the thread to finish 
+print(f'{ctime()} Waiting for the thread to finish') #ทำงานเลยไม่รอ
+thread.join()
--- assignment1/thread04.py
+++ assignment1/thread04.py
@@ -1,1 +1,27 @@
 # extending the Thread class and return values
+from time import sleep, ctime
+from threading import Thread
+
+class CustomThread(Thread):
+    #override the run function
+    def run(self):
+        #block for a moment
+        sleep(1)
+        #Display a message
+        print(f'{ctime()} This is coming from another thread')
+        #store return value
+        self.value = 99
+
+#Create a thread
+thread = CustomThread()
+#Run the thread
+thread.start()
+'''
+Thread 2nd
+'''
+#Wait for the thread to finish 
+print(f'{ctime()} Waiting for the thread to finish') #ทำงานเลยไม่รอ
+thread.join()
+#get the value returned from run
+value = thread.value
+print(f'{ctime()} Got : {value}')
