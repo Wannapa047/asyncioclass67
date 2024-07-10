23738f6797684c24f4faa581fc28ebe169a80b48
Wannapa047 <153887983+Wannapa047@users.noreply.github.com>
2024-07-10 12:07:46 +0700

    asyncio05

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -3,37 +3,40 @@ import random
 
 # coroutine สำหรับทำอาหาร
 async def cook_rice():
-    cook_time = 1 + random.random() * 10
+    cook_time = 1 + random.random()
     await asyncio.sleep(cook_time)
     print(f"Rice is ready in {cook_time:.2f} seconds")
-    return ("rice", cook_time)
+    return "rice", cook_time
 
 async def cook_noodle():
-    cook_time = 1 + random.random() * 10
+    cook_time = 1 + random.random()
     await asyncio.sleep(cook_time)
     print(f"Noodle is ready in {cook_time:.2f} seconds")
-    return ("noodle", cook_time)
+    return "noodle", cook_time
 
 async def cook_curry():
-    cook_time = 1 + random.random() * 10
+    cook_time = 1 + random.random()
     await asyncio.sleep(cook_time)
     print(f"Curry is ready in {cook_time:.2f} seconds")
-    return ("curry", cook_time)
+    return "curry", cook_time
 
 # main coroutine
 async def main():
     # สร้าง tasks สำหรับทำอาหารแต่ละชนิด
     tasks = [
-        cook_rice(),
-        cook_noodle(),
-        cook_curry()
+        asyncio.create_task(cook_rice()),
+        asyncio.create_task(cook_noodle()),
+        asyncio.create_task(cook_curry())
     ]
-    # รอให้ tasks เสร็จสมบูรณ์ทีละงาน
-    results = await asyncio.gather(*tasks)
-
+    # รอให้ tasks เมนูแรกเสร็จสมบูรณ์แล้วนำไปกิน
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    first_result = None
+    for task in done:
+        first_result, cook_time = await task
+    for task in pending:
+        await task
     
-    first_done = min(results, key=lambda x: x[1])
-    print(f"Student A eats {first_done[0]} which was ready in {first_done[1]:.2f} seconds.")
+    print(f"Student A eats {first_result}")
 
 # เริ่มโปรแกรม asyncio
 asyncio.run(main())
