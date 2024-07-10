8c1dec2a51353cca0d04a18323e21841d5d272bf
Wannapa047 <153887983+Wannapa047@users.noreply.github.com>
2024-07-10 11:11:14 +0700

    Cook

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -2,23 +2,23 @@ import asyncio
 import random
 
 # coroutine สำหรับทำอาหาร
-async def cook_rice(): #เป็นฟังก์ชันแบบ asynchronous ซึ่งใช้เวลาสุ่มระหว่าง 1 ถึง 11 วินาที (1 + random.random() * 10)
+async def cook_rice():
     cook_time = 1 + random.random() * 10
-    await asyncio.sleep(cook_time) #หยุดการทำงานของ coroutine นี้เป็นระยะเวลาที่สุ่มได้ เพื่อจำลองเวลาที่ใช้ในการทำอาหาร
+    await asyncio.sleep(cook_time)
     print(f"Rice is ready in {cook_time:.2f} seconds")
-    return "rice"
+    return ("rice", cook_time)
 
 async def cook_noodle():
     cook_time = 1 + random.random() * 10
     await asyncio.sleep(cook_time)
     print(f"Noodle is ready in {cook_time:.2f} seconds")
-    return "noodle"
+    return ("noodle", cook_time)
 
 async def cook_curry():
     cook_time = 1 + random.random() * 10
     await asyncio.sleep(cook_time)
     print(f"Curry is ready in {cook_time:.2f} seconds")
-    return "curry"
+    return ("curry", cook_time)
 
 # main coroutine
 async def main():
@@ -29,11 +29,11 @@ async def main():
         cook_curry()
     ]
     # รอให้ tasks เสร็จสมบูรณ์ทีละงาน
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        print(f"Student A eats {result}")
-        break  # หยุดหลังจากเมนูแรกเสร็จ
+    results = await asyncio.gather(*tasks)
+
+    
+    first_done = min(results, key=lambda x: x[1])
+    print(f"Student A eats {first_done[0]} which was ready in {first_done[1]:.2f} seconds.")
 
 # เริ่มโปรแกรม asyncio
-asyncio.run(main()) #รันฟังก์ชัน main ซึ่งเป็นจุดเริ่มต้นของโปรแกรมที่ใช้ asyncio
-
+asyncio.run(main())
