7f90d16b99a8fc0a33453eac37dcbdf97f08cab8
Wannapa047 <153887983+Wannapa047@users.noreply.github.com>
2024-08-21 09:57:24 +0700

    Assignment9

---
+++ assignment09/taskgroup02.py
@@ -0,0 +1,26 @@
+# example of asyncio task group with return values
+import asyncio
+
+# coroutine task
+async def task(value):
+    # sleep to simulate waiting
+    await asyncio.sleep(1)
+    # return value
+    return value * 100
+
+# asyncio entry point
+async def main():
+    # create task group
+    async with asyncio.TaskGroup() as group:
+        # create and issue tasks
+        tasks = [group.create_task(task(i)) for i in range(1, 10)]
+        
+    # wait for all tasks to complete and gather results
+    results = [await t for t in tasks]
+    
+    # report all results
+    for result in results:
+        print(result)
+
+# entry point
+asyncio.run(main())
