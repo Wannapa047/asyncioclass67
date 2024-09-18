86422d420f1a41708036b0d777d91b480f6dfea4
Wannapa047 <153887983+Wannapa047@users.noreply.github.com>
2024-09-18 12:09:31 +0700

    Bonus

--- bonus/consumer.py
+++ bonus/consumer.py
@@ -1,1 +1,23 @@
 # https://aiokafka.readthedocs.io/en/stable/
+import aiokafka
+from aiokafka import AIOKafkaConsumer
+import asyncio
+import random
+
+async def consume():
+    consumer = AIOKafkaConsumer(
+        'my_topic', 'my_other_topic',
+        bootstrap_servers='localhost:9092',
+        group_id="my-group")
+    # Get cluster layout and join group `my-group`
+    await consumer.start()
+    try:
+        # Consume messages
+        async for msg in consumer:
+            print("consumed: ", msg.topic, msg.partition, msg.offset,
+                  msg.key, msg.value, msg.timestamp)
+    finally:
+        # Will leave consumer group; perform autocommit if enabled.
+        await consumer.stop()
+
+asyncio.run(consume())
--- bonus/producer.py
+++ bonus/producer.py
@@ -1,1 +1,25 @@
-# https://aiokafka.readthedocs.io/en/stable/
+import aiokafka
+from aiokafka import AIOKafkaProducer
+import asyncio
+import json  # สำหรับแปลง dict เป็น bytes
+
+async def send_one():
+    producer = AIOKafkaProducer(
+        bootstrap_servers='localhost:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')  # แปลง dict เป็น bytes
+    )
+    # Get cluster layout and initial topic/partition leadership information
+    await producer.start()
+    try:
+        while True:
+            data = {"a": 123.4, "b": "some string"}
+            # ส่งข้อมูลไปยัง Kafka โดยใช้ send_and_wait
+            res = await producer.send_and_wait(topic="my_topic", value=data)
+            print(f"Message sent: {res}")
+            await asyncio.sleep(1)
+    finally:
+        # หยุด producer เมื่อเสร็จสิ้นการทำงาน
+        await producer.stop()
+
+# เริ่มรันฟังก์ชัน
+asyncio.run(send_one())
